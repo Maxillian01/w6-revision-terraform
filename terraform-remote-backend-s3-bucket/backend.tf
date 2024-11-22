95116525128d263cf3f7b8d6e9dddabb2b1c8d03
Maxillian01 <memaxillan01@gmail.com>
2024-11-22 08:37:58 -0600

    change

--- terraform-remote-backend-s3-bucket/backend.tf
+++ terraform-remote-backend-s3-bucket/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "week6-se-terraform-bucket"
+    bucket = "week6-se-my-bucket"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "my-lock"
     encrypt = true 
   }
 }
+
